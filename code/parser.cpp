be7af307b5a22087e2003cd8c908a4eab92bc25a
Aleksej Godenko <alexgodworkd@gmail.com>
2026-06-05 03:55:07 +0300

    git commit -m "init help func"

---
+++ code/parser.cpp
@@ -0,0 +1,27 @@
+#include "parser.h"
+
+#include <memory>
+#include <vector>
+
+const Token& Parser::currentToken() const {
+    return tokens[position];
+}
+
+bool Parser::match(TokenType type) const {
+    return currentToken().type == type;
+}
+
+bool Parser::matchOperation(OperationType operation) const {
+    return currentToken().type == TokenType::Operation &&
+           currentToken().operation == operation;
+}
+
+bool Parser::isEnd() const {
+    return currentToken().type == TokenType::End;
+}
+
+void Parser::moveNext() {
+    if (position + 1 < tokens.size()) {
+        ++position;
+    }
+}
